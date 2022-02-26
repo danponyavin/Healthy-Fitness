27d5bc5704258198a4e776a1cd6191d97fe790ff
Daniil <dan.ponyavin@yandex.ru>
2022-02-26 12:11:24 +0300

    Template created and base pages added

---
+++ healthyfitness/main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.main_page, name='home'),
+    path('articles', views.articles, name='articles'),
+    path('calculator', views.calculator, name='calculator'),
+    path('blog', views.blog, name='blog'),
+    path('food_diary', views.food_diary, name='food_diary'),
+    path('personal_area', views.personal_area, name='personal_area'),
+]
---
+++ healthyfitness/main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+def main_page(request):
+    return render(request, 'main/main_page.html')
+
+
+def articles(request):
+    return render(request, 'main/articles.html')
+
+
+def calculator(request):
+    return render(request, 'main/calculator.html')
+
+
+def blog(request):
+    return render(request, 'main/blog.html')
+
+
+def food_diary(request):
+    return render(request, 'main/food_diary.html')
+
+
+def personal_area(request):
+    return render(request, 'main/personal_area.html')
