450d5dfebde28803818a3399e51bc365f2ad733a
Natalie <lunatalied@yandex.ru>
2022-03-15 19:51:04 +0300

    Added diffetent pages for articles

---
+++ healthyfitness/registration/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def registration_field(request):
+    return render(request, 'registration/registration_field.html')
+
+
+def login(request):
+    return render(request, 'registration/login.html')
