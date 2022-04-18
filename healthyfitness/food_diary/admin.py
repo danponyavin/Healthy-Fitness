ab4292fa5e6ea31a03d7fdc5cbcaf36bcf8dd3de
Helen <eivorobeva@edu.hse.ru>
2022-04-18 23:41:08 +0300

    create db for for and type of food and admin panel setup

--- healthyfitness/food_diary/admin.py
+++ healthyfitness/food_diary/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Type_of_food
+from .models import Food
 
-# Register your models here.
+
+class Type_of_foodAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'photo')
+    list_display_links = ('id', 'type')
+    search_fields = ('type',)
+    prepopulated_fields = {"slag": ("type",)}
+
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name_of_product', 'kkal', 'proteins', 'fats', 'carbohydrates')
+    list_display_links = ('id', 'name_of_product')
+    search_fields = ('name_of_product',)
+    prepopulated_fields = {"slag": ("name_of_product",)}
+
+
+admin.site.register(Type_of_food, Type_of_foodAdmin)
+admin.site.register(Food, FoodAdmin)
+
+
--- healthyfitness/food_diary/urls.py
+++ healthyfitness/food_diary/urls.py
@@ -1,7 +1,12 @@
+from django.conf.urls.static import static
 from django.urls import path
+from healthyfitness import settings
 from . import views
 
 urlpatterns = [
     path('personal_area/food_diary', views.UserFood, name='food_diary'),
     path('personal_area/product_selection', views.ProductSelection, name='product_selection'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
