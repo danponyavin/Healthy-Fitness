81e9e18ff6d72e7310ff2b6b5c77d4ebefdeef1a
Natalie <lunatalied@yandex.ru>
2022-04-20 20:01:39 +0300

    created pages for types of products

--- healthyfitness/food_diary/urls.py
+++ healthyfitness/food_diary/urls.py
@@ -6,6 +6,14 @@ from . import views
 urlpatterns = [
     path('personal_area/food_diary', views.UserFood, name='food_diary'),
     path('personal_area/product_selection', views.ProductSelection, name='product_selection'),
+    path('personal_area/product_selection/bakery', views.Bakery, name='bakery'),
+    path('personal_area/product_selection/cereals', views.Cereals, name='cereals'),
+    path('personal_area/product_selection/meat', views.Meat, name='meat'),
+    path('personal_area/product_selection/vegFruit', views.VegFruit, name='vegFruit'),
+    path('personal_area/product_selection/seafood', views.Seafood, name='seafood'),
+    path('personal_area/product_selection/cookedMeals', views.CookedMeals, name='cookedMeals'),
+    path('personal_area/product_selection/milk', views.Milk, name='milk'),
+    path('personal_area/product_selection/sweets', views.Sweets, name='sweets'),
 ]
 
 if settings.DEBUG:
