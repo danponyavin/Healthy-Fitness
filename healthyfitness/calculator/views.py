ccf3dda1cc8659e716059a2e5b766c38b299aee3
Daniil <dan.ponyavin@yandex.ru>
2022-04-21 18:30:27 +0300

    Added processing of the save result button in the calculator

--- healthyfitness/calculator/views.py
+++ healthyfitness/calculator/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from calculator.forms import CalculatorForm
 
@@ -14,6 +14,29 @@ def isfloat(value):
     except ValueError:
         return False
 
+def get_user_data(data):
+    if data["gender"] == 1:
+        db_user_data["gender"] = "Мужчина"
+    else:
+        db_user_data["gender"] = "Женщина"
+    if data["activity"] == 1:
+        db_user_data["activity_level"] = "Отсутствие активности"
+    elif data["activity"] == 2:
+        db_user_data["activity_level"] = "Низкая активность"
+    elif data["activity"] == 3:
+        db_user_data["activity_level"] = "Средняя активность"
+    elif data["activity"] == 4:
+        db_user_data["activity_level"] = "Высокая активность"
+    elif data["activity"] == 5:
+        db_user_data["activity_level"] = "Экстремальная активность"
+    if data["aim"] == 1:
+        db_user_data["user_aim"] = "Похудение"
+    elif data["aim"] == 2:
+        db_user_data["user_aim"] = "Поддержание веса"
+    elif data["aim"] == 3:
+        db_user_data["user_aim"] = "Набор мышечной массы"
+    return db_user_data
+
 
 def calcUserData(data):
     activity_index = {1: 1.2, 2: 1.375, 3: 1.55, 4: 1.725, 5: 1.9}
@@ -28,12 +51,16 @@ def calcUserData(data):
     proteins = round(calories * indicators[data["aim"]][0] / 4)
     fats = round(calories * indicators[data["aim"]][1] / 9)
     carbohydrates = round(calories * indicators[data["aim"]][2] / 4)
-
+    db_user_data = {'calories': calories, 'proteins': proteins, 'fats': fats, 'carbohydrates': carbohydrates}
     result = {'calories': calories, 'proteins': proteins, 'fats': fats, 'carbohydrates': carbohydrates}
     return result
 
 
+db_user_data = {'age': 0, 'weight': 0, 'gender': "", 'growth': 0, 'activity_level': "", 'user_aim': "",
+                'calories': 0, 'proteins': 0, 'fats': 0, 'carbohydrates': 0}
+
 def calculator(request):
+    global db_user_data
     is_valid = False
     error_info = False
     IMT = 0
@@ -43,7 +70,6 @@ def calculator(request):
     proteins = ''
     fats = ''
     carbohydrates = ''
-
     if request.method == 'POST':
         if form.is_valid():
             temp = request.POST
@@ -56,15 +82,22 @@ def calculator(request):
                 aim = int(temp["user_aim"])
                 gender = int(temp["gender"])
                 if 59 < growth < 231 and 9 < age < 101 and 25 < weight < 210:
+                    db_user_data = {'age': age, 'growth': growth, 'weight': weight}
                     user_data = {'age': age, 'growth': growth, 'weight': weight, 'activity': activity,
                                  'aim': aim, 'gender': gender}
                     user_data_res = calcUserData(user_data)
+                    get_user_data(user_data)
                     IMT = calcIMT(growth, weight)
                     calories = str(user_data_res['calories'])
                     proteins = str(user_data_res['proteins'])
                     fats = str(user_data_res['fats'])
                     carbohydrates = str(user_data_res['carbohydrates'])
                     is_valid = True
+
+    if "save_data" in request.POST:
+        print(db_user_data)
+        return redirect('home')
+
     cont = {'res': is_valid,
             'error': error_info,
             'form': form,
