2a052d47ee4cd4dc977640128d157c7e588dfa4f
Helen <eivorobeva@edu.hse.ru>
2022-05-06 15:44:47 +0300

    created db for water tracker

--- healthyfitness/water_tracker/admin.py
+++ healthyfitness/water_tracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Water_tracker
 
-# Register your models here.
+class Water_trackerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'id_users', 'day_create', 'number_of_glasses')
+    list_display_links = ('id', 'id_users', 'day_create')
+    search_fields = ('id_users', 'day_create')
+
+
+admin.site.register(Water_tracker, Water_trackerAdmin)
--- healthyfitness/water_tracker/models.py
+++ healthyfitness/water_tracker/models.py
@@ -1,3 +1,16 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+class Water_tracker(models.Model):
+    id_users = models.ForeignKey('calculator.Profile', on_delete=models.PROTECT)
+    day_create = models.DateField(auto_now_add=True)
+    number_of_glasses = models.IntegerField(validators=[MaxValueValidator(50)])
+
+    def __str__(self):
+        return str(self.number_of_glasses)
+
+    class Meta:
+        verbose_name = 'Трекер воды'
+        verbose_name_plural = 'Трекер воды'
+        ordering = ['day_create']
+
