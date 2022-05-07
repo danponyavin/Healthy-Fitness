cfc622a326e3856384f3ef2a5c0c94eaa1c350e4
Natalie <lunatalied@yandex.ru>
2022-05-07 15:53:21 +0300

    created form for water

--- healthyfitness/water_tracker/views.py
+++ healthyfitness/water_tracker/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from water_tracker.forms import WaterForm
 
 
 def AddWater(request):
-    return render(request, 'water_tracker/add_water.html')
+
+    if request.method == 'POST':
+        form = WaterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_water')
+    else:
+        form = WaterForm
+
+    return render(request, 'water_tracker/add_water.html', {'form': form,})
 
 
 def WaterTracker(request):
