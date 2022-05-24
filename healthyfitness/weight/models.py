210309c71db86647a8069e1c247e1c972a33ed23
Helen <eivorobeva@edu.hse.ru>
2022-05-24 22:15:07 +0300

    create data base for weigh_tracker

--- healthyfitness/weight/models.py
+++ healthyfitness/weight/models.py
@@ -1,3 +1,16 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Weight_trecker(models.Model):
+    id_users = models.ForeignKey('calculator.Profile', on_delete=models.PROTECT)
+    day_create = models.DateField(auto_now_add=True)
+    weight = models.FloatField(null=True, validators=[MaxValueValidator(400)])
+
+    def __str__(self):
+        return str(self.weight)
+
+    class Meta:
+        verbose_name = 'Трекер веса'
+        verbose_name_plural = 'Трекер веса'
+        ordering = ['day_create']
