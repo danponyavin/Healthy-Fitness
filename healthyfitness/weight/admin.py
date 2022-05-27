235d699e48c8044875335acf9d09f4378df96ba3
Helen <eivorobeva@edu.hse.ru>
2022-05-28 01:05:37 +0300

    created db for sport tracker and add list of training

--- healthyfitness/weight/admin.py
+++ healthyfitness/weight/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Weight_trecker
 
+
 class Weight_treckerAdmin(admin.ModelAdmin):
     list_display = ('id', 'id_users', 'day_create', 'weight')
     list_display_links = ('id', 'id_users', 'day_create')
