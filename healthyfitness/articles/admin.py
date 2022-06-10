b58d82cc666165bbddee12b700a39a292e020eb7
Natalie <lunatalied@yandex.ru>
2022-06-10 19:14:56 +0300

    Code improvements

--- healthyfitness/articles/admin.py
+++ healthyfitness/articles/admin.py
@@ -1,4 +1,4 @@
-from typing import Dict, Tuple
+"""Contains admin-panel classes"""
 
 from django.contrib import admin
 from .models import Category
@@ -6,6 +6,7 @@ from .models import Article
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    """Admin for article categories"""
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
@@ -13,6 +14,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 class ArticleAdmin(admin.ModelAdmin):
+    """Admin for articles"""
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
@@ -22,5 +24,3 @@ class ArticleAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Article, ArticleAdmin)
-
-
--- healthyfitness/articles/apps.py
+++ healthyfitness/articles/apps.py
@@ -1,6 +1,9 @@
+"""Declare new app"""
+
 from django.apps import AppConfig
 
 
 class ArticlesConfig(AppConfig):
+    """Config for app"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'articles'
